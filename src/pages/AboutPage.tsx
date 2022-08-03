cb037d87a911e048f97c666a323d1596472acb2f
Denys <96637006+DenysSkobalo@users.noreply.github.com>
2022-08-03 23:26:29 +0300

    Public

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cupiditate, delectus dolor error et in nisi odit omnis optio praesentium quae quasi quibusdam similique temporibus vel velit voluptatibus. Ab adipisci amet assumenda, deserunt, fuga impedit laborum obcaecati perspiciatis possimus provident quo recusandae temporibus vel! Aperiam aspernatur exercitationem incidunt mollitia officia?</div>
+    )
+}
